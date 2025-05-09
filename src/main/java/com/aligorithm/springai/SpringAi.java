cc1a3cde4fcd38afb216a66f42ec93539d24b464
ali <atemurtas97@gmail.com>
2025-05-09 16:53:43 +0300

    aligorithm-springAi

---
+++ src/main/java/com/aligorithm/springai/SpringAi.java
@@ -0,0 +1,13 @@
+package com.aligorithm.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAi.class, args);
+    }
+
+}
---
+++ src/test/java/com/aligorithm/springai/AiWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aligorithm.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
